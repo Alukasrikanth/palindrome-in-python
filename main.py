73e218780e5f3257a0b65b42c237038ea80cf0a2
ALUKA SRIKANTH <94375466+Alukasrikanth@users.noreply.github.com>
2022-09-02 17:10:20 +0000

    no changes

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 name=input("Enter Your name:")
-print(name)
+string=name
+palindrome=''
+for word in name[::-1]:
+  palindrome+=word 
+if palindrome== string:
+  print("Palindrome")
+else:
+   print("Not Palindrome")
